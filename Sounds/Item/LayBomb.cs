5c69529c22ccc7338fba7940f148e0f72aeccdc3
Joost8910 <bluedog@fenria.com>
2019-08-27 20:57:21 -0500

    Add files via upload

---
+++ Sounds/Item/LayBomb.cs
@@ -0,0 +1,19 @@
+using Microsoft.Xna.Framework.Audio;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace JoostMod.Sounds.Item
+{
+	public class LayBomb : ModSound
+	{
+		public override SoundEffectInstance PlaySound(ref SoundEffectInstance soundInstance, float volume, float pan, SoundType type)
+		{
+			soundInstance = sound.CreateInstance();
+			soundInstance.Volume = volume * .5f;
+			soundInstance.Pan = pan;
+			soundInstance.Pitch = 0f;
+			Main.PlaySoundInstance(soundInstance);
+			return soundInstance;
+		}
+	}
+}
